82240af5bef118fc90efcf01ff4d945142a4a7fc
Fabricio Lopes <Fabriciooliveiralopes50@gmail.com>
2022-05-21 22:26:55 -0300

    a

--- src/App.js
+++ src/App.js
@@ -6,32 +6,27 @@ function App() {
   return (
     <div>
       <header>
-        <h1>Titulo</h1>
-      <nav>
+        <h1>Fabrício Lopes</h1>
         <a className='link' href='#home'>Home</a>
         <a className='link' href='#sobre'>Sobre</a>
-        <a className='link' href='#exp'>Experiência</a>
         <a className='link' href='#proj'>Projetos</a>
         <a className='link' href='#cont'>Contato</a>
-      </nav>
       </header>
       <main>
-        <section className='principal' id='home'>
-          <p className='me'>
-            Olá, eu sou <strong>Fabrício Lopes</strong>, Desenvolvedor Front-End Junior!
+        <section className='home' id='home'>
+          <p className='eu'>
+            Olá, eu sou <strong>Fabrício Lopes</strong>, <br/> Desenvolvedor Front-End Junior!
           </p>
-          <img src={logo} className="pic"></img>
+          <img src={logo} className="photo" />
         </section>
           <h2 id='sobre'>Quem sou eu?</h2>
           <h2>São Paulo, Brasil</h2>
           <p>
             Olá, tenho 19 anos, com um pouco de conhecimento em Inglês(avançado) e aprendendo Japonês. Com conhecimentos básicos em HTML5, CSS3, JavaScript e ReactJS.
-          </p>
+            Ainda sem experiência, pois estou estudando para poder conseguir um emprego na área da programação. De começo como Desenvolvedor Front-End Junior</p>
           <a href={curriculo} download="Curriculo Fabrício Lopes">Currículo</a>
       </main>
       <section>
-        <h2 id='exp'>Experiência</h2>
-        <p>Ainda sem experiência, pois estou estudando para poder conseguir um emprego na área da programação. De começo como Desenvolvedor Front-End Junior</p>
         <h2 id='proj'>Projetos</h2>
         <h2 id='cont'>Contato</h2>
         <a href='https://www.linkedin.com/in/fabricio-lopes1325' target="_blank">LinkedIn</a>
